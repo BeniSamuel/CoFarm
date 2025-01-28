7cd163886545204a4d5e6b51ce38b6f827b0a28c
benisamuel <benisamuel566@gmail.com>
2025-01-28 17:07:01 +0200

    Improved UI

--- src/Components/Dashgraph/Dashgraph.jsx
+++ src/Components/Dashgraph/Dashgraph.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Dashgraph = () => {
   return (
-    <div className=' bg-[#c2f0bb] w-[55vw] h-80 rounded-lg'>
+    <div className=' flex flex-col gap-1 w-[55vw]'>
+      <div className=' text-sm font-semibold'>Overview</div>
+      <div className=' bg-[#c2f0bb] h-80 rounded-lg'>
 
+      </div>
     </div>
   )
 }
--- src/Components/Farms/FarmRight.jsx
+++ src/Components/Farms/FarmRight.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
 import Dashtop from "../Dashtop/Dashtop";
 import FarmToggle from "../FarmToggle/FarmToggle";
+import empty from "../../assets/empty.svg"
 
 const FarmRight = () => {
   const [ tabActive, setTabActive ] = useState("farms");
+  const tableFarms = null;
   return (
     <div className=" w-5/6">
       <Dashtop />
       <div>
         <FarmToggle tabActive={tabActive} setTabActive={setTabActive}/> 
+        {
+          tableFarms === null ? 
+          (
+            <div className=" flex flex-col items-center py-12">
+              <img src={empty} className=" h-44 w-44"/>
+              <p className=" text-sm text-green-900">No availabe farms</p>
+            </div>
+          )
+          : 
+          ""
+        }
+
       </div>
     </div>
   );
