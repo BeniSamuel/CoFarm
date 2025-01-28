6547eb147aafa4feba3ffd974c74a2d37b2376cf
benisamuel <benisamuel566@gmail.com>
2025-01-28 13:22:27 +0200

    Modified Dashtop Topnavbar

--- src/Components/Dashtop/Dashtop.jsx
+++ src/Components/Dashtop/Dashtop.jsx
@@ -4,13 +4,13 @@ import profile_img from "../../assets/profile_image.png";
 
 const Dashtop = () => {
   return (
-    <div className=" bg-[#83DF75] flex flex-row justify-between items-center px-6 w-full h-16">
+    <div className=" bg-[#83DF75] flex flex-row justify-between items-center px-6 w-full h-14">
       <div>
-        <img src={menu_icon} className=" h-10 w-10" />
+        <img src={menu_icon} className=" h-7 w-7" />
       </div>
       <div>
-        <img src={profile_img} className=" h-10 w-10" />
-        <div className=" bg-green-700 h-3 w-3 rounded-full relative bottom-3 left-8"></div>
+        <img src={profile_img} className=" h-9 w-9" />
+        <div className=" bg-green-900 h-2 w-2 rounded-full relative bottom-2 left-7 "></div>
       </div>
     </div>
   );
