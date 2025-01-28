1062651138cd761131b277055acecdaa4673201e
benisamuel <benisamuel566@gmail.com>
2025-01-28 15:24:55 +0200

    Improved UserDisplayProfile

--- src/Components/ProfileChat/ProfileChat.jsx
+++ src/Components/ProfileChat/ProfileChat.jsx
@@ -8,23 +8,20 @@ const ProfileChat = () => {
 
   if (!currentUser)
     return (
-      <div className=" w-2/6 bg-[#83DF75] flex flex-col items-center pt-3">
+      <div className=" w-2/6 bg-[#83DF75] flex flex-col items-center justify-center pt-3 text-sm">
         Select a user to view their profile
       </div>
     );
 
   return (
-    <div className="w-2/6 bg-[#83DF75] flex flex-col items-center pt-3">
-      <div>
-        <h2>{currentUser.name}</h2>
-        <p>{currentUser.email}</p>
-      </div>
-      <div>
+    <div className="w-2/6 bg-[#83DF75] flex flex-col items-center pt-3 gap-8 ">
+      <div className=" flex flex-col items-center text-sm font-semibold">Information</div>
+      <div className=" flex flex-col items-center h-40 w-40 bg-[#a9ea9f] rounded-full">
         <img src="" alt="Profile" />
       </div>
-      <div>
-        <div></div>
-        <div></div>
+      <div className=" flex flex-col justify-center items-center">
+        <h2 className=" text-sm ">{currentUser.name}</h2>
+        <p className=" text-sm ">{currentUser.email}</p>
       </div>
     </div>
   );
