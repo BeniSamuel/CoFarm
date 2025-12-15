7bf84e04c6bb73f5f9a52c5d725539cac5c89eda
benisamuel <benisamuel566@gmail.com>
2025-12-15 19:54:08 +0200

    working on improved functionalities

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// index.js
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import ChatProvider from "./Context/ChatContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // Remove React.StrictMode
-  <ChatProvider>
-    <App />
-  </ChatProvider>
+  <StrictMode>
+    <ChatProvider>
+      <App />
+    </ChatProvider>
+  </StrictMode>
 );
