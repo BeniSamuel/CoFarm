eefdd361615ffc929d3fac393624f1ef6b1b401d
benisamuel <benisamuel566@gmail.com>
2025-12-15 19:51:23 +0200

    added adding farm inform functionalities

--- src/Pages/Farms/Farm.jsx
+++ src/Pages/Farms/Farm.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 import Dashnav from "../../Components/Dashnav/Dashnav";
 import FarmRight from "../../Components/Farms/FarmRight";
 
 const Farm = () => {
   return (
-    <div className=" flex flex-row">
+    <div className="flex flex-row w-full min-h-screen">
       <Dashnav />
-      <FarmRight />
+      <div className="flex-1 overflow-hidden">
+        <FarmRight />
+      </div>
     </div>
   );
 };
