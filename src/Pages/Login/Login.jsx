31e533fa81bdeed3a8b6a3c93cc7c9de7271fa22
benisamuel <benisamuel566@gmail.com>
2025-12-15 19:52:18 +0200

    fixed eslint errors

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Loginleft from "../../Components/Login-Left/Loginleft";
 import { Link, useNavigate } from "react-router-dom";
@@ -11,11 +11,14 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:4040/api/v1/auth/login", inform );
+      const response = await axios.post(
+        "http://localhost:4040/api/v1/auth/login",
+        inform
+      );
 
       const token = response.data.access_token;
       console.log(token);
-      localStorage.setItem("accessToken",token);
+      localStorage.setItem("accessToken", token);
       toast.success("Successfully logged in");
       navigate("/dashboard"); // Navigate to dashboard
     } catch (error) {
@@ -38,13 +41,15 @@ const Login = () => {
           <input
             type="email"
             placeholder="Email"
-            className="border-2 border-[#184B05] h-12 w-96 md:h-[3.2rem] md:w-96 pl-4 placeholder-[#49881F] placeholder:text-sm rounded-lg text-sm text-[##49881F]"
+            value={inform.email}
+            className="border-2 border-[#184B05] h-12 w-96 md:h-[3.2rem] md:w-96 pl-4 placeholder-[#49881F] placeholder:text-sm rounded-lg text-sm text-[#49881F]"
             onChange={(e) => setInform({ ...inform, email: e.target.value })}
           />
           <input
             type="password"
             placeholder="Password"
-            className="border-2 border-[#184B05] h-12 w-96 md:h-[3.2rem] md:w-96 pl-4 placeholder-[#49881F] placeholder:text-sm rounded-lg text-sm text-[##49881F]"
+            value={inform.password}
+            className="border-2 border-[#184B05] h-12 w-96 md:h-[3.2rem] md:w-96 pl-4 placeholder-[#49881F] placeholder:text-sm rounded-lg text-sm text-[#49881F]"
             onChange={(e) => setInform({ ...inform, password: e.target.value })}
           />
           <p className="text-[#184B05] font-semibold text-right text-sm">
@@ -58,7 +63,9 @@ const Login = () => {
           <p className="text-[#49881F] text-center text-sm">
             New to the site?{" "}
             <Link to="/signup">
-              <span className="text-[#184B05] font-semibold text-sm">Sign Up</span>
+              <span className="text-[#184B05] font-semibold text-sm">
+                Sign Up
+              </span>
             </Link>
           </p>
         </form>
